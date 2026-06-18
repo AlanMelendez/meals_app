b0933eb7fb517898e2d6065844a27c5e141a594b
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2026-06-17 22:22:59 -0600

    feat: Add MealItem widget and update MealsScreen to use it

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
@@ -11,10 +12,43 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(28)
+      ),
+      clipBehavior: Clip.hardEdge, 
+      elevation: 5, //Shadow like BoxShadow in CSS
       child: InkWell(
         onTap: (){},
         child: Stack(
           children: [
+            FadeInImage(placeholder: MemoryImage(kTransparentImage), image: NetworkImage(meal.imageUrl)),
+            Positioned( top: 0, bottom: 0, left: 0, right: 0,child: Container(
+              color: Colors.black54,
+              padding: const EdgeInsets.symmetric(vertical:6, horizontal:44),
+              child:  Column(
+                children: [
+                  Text(
+                    meal.title,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                   ),
+                   const SizedBox(height: 12),
+                   Row(
+                    children: [
+
+                    ],
+                   )
+                ],
+              ),
+            ),)
           ],
         ),
       ),
