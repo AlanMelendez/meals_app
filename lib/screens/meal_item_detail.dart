8392e2f52ff7a2fac6440224c47d12848c9c18f6
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2026-07-16 18:44:07 -0600

    feat: Refactor MealDetailsScreen to display ingredients and steps in a scrollable view

--- lib/screens/meal_item_detail.dart
+++ lib/screens/meal_item_detail.dart
@@ -10,11 +10,57 @@ class MealDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(meal.title)),
-        body: Image.network(
-          meal.imageUrl,
-          width: double.infinity,
-          height: 300,
-          fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                'Ingredients',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final ingredient in meal.ingredients)
+                Text(
+                  ingredient,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+                ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                'Steps',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+              ),
+              for (final step in meal.steps)
+                Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+                ),
+            ],
+          ),
         ));
   }
 }
